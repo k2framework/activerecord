fb0fe7ab0dc62018f3d5d4d285c3add019c295f4
Manuel Aguirre <programador.manuel@gmail.com>
2012-11-12 16:57:04 -0430

    agregando tests de Metadata...

---
+++ unit-tests/MetadataTest.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once __DIR__ . '/autoload.php';
+
+use ActiveRecord\Metadata\Metadata;
+use ActiveRecord\Metadata\Attribute;
+
+class MetadataTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testAtribute()
+    {
+        $attr = new Attribute();
+
+        $this->assertFalse($attr->FK);
+        $this->assertFalse($attr->PK);
+        $this->assertFalse($attr->unique);
+        $this->assertFalse($attr->autoIncrement);
+        $this->assertNull($attr->alias);
+        $this->assertNull($attr->default);
+        $this->assertNull($attr->format);
+        $this->assertTrue($attr->notNull);
+        $this->assertNull($attr->type);
+        $this->assertEquals(50, $attr->length);
+    }
+
+    public function testMetadata()
+    {
+        
+    }
+
+}
