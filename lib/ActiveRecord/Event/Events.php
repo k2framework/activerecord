22a6ad5b6be5788fea95e6adaf98850e18ce69e1
Manuel Aguirre <programador.manuel@gmail.com>
2012-12-03 23:21:06 -0430

    agregando clase Events con los posibles eventos a ejecutar...

---
+++ lib/ActiveRecord/Event/Events.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ActiveRecord\Event;
+
+/**
+ * Description of KumbiaEvents
+ *
+ * @author manuel
+ */
+final class Events
+{
+
+    const BEFORE_QUERY = 'activerecord.beforequery';
+    const BEFORE_SAVE = 'activerecord.beforequery';
+    const BEFORE_UPDATE = 'activerecord.beforequery';
+    const BEFORE_CREATE = 'activerecord.beforequery';
+    const BEFORE_DELETE = 'activerecord.beforequery';
+    const BEFORE_SELECT = 'activerecord.beforequery';
+    const AFTER_QUERY = 'activerecord.afterquery';
+    const AFTER_SAVE = 'activerecord.afterquery';
+    const AFTER_UPDATE = 'activerecord.afterquery';
+    const AFTER_CREATE = 'activerecord.afterquery';
+
+}
